f055c13819ea4da769e13f9a2a108edbe9efdaa2
mathimaran2510-a11y <mathimaran2510@gmail.com>
2026-06-08 16:56:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
     return {"message": "API Working"}
 
 @app.post("/analyze")
-def analyze():
+async def analyze(audio: UploadFile = File(...)):
+    
+    # Just proving the upload works
+    file_name = audio.filename
+
     return {
         "stress_level": "Moderate",
         "emotion": "Neutral",
-        "confidence": 82
+        "confidence": 82,
+        "received_file": file_name
     }
